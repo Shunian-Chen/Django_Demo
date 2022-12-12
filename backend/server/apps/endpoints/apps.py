6f966544c7d3bb6cccbadda696a38786c73ae1b5
Hank <40827070+Shunian-Chen@users.noreply.github.com>
2022-12-12 06:08:26 +0000

    Django version updated

--- backend/server/apps/endpoints/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class EndpointsConfig(AppConfig):
-    name = 'apps.endpoints'
-    default_auto_field = 'django.db.models.BigAutoField'
--- djangoDemo/mysite/mysite/urls.py
+++ djangoDemo/mysite/mysite/urls.py
@@ -1,7 +1,7 @@
-"""server URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# backend/server/server/urls.py file
 from django.contrib import admin
 from django.urls import path
 from apps.endpoints.urls import urlpatterns as endpoints_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
 
 urlpatterns += endpoints_urlpatterns
