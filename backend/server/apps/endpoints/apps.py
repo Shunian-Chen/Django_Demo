b37066cd18f62f2218326e2da10d1a9af6634bed
Hank <40827070+Shunian-Chen@users.noreply.github.com>
2022-12-12 05:39:49 +0000

    Django Models

--- backend/server/apps/endpoints/apps.py
+++ backend/server/apps/endpoints/apps.py
@@ -2,4 +2,5 @@ from django.apps import AppConfig
 
 
 class EndpointsConfig(AppConfig):
-    name = 'endpoints'
+    name = 'apps.endpoints'
+    default_auto_field = 'django.db.models.BigAutoField'
